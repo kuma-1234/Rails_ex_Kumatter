9d4d34c77f1efad06c0356b7df00b4e60d99bfa3
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-09-30 14:09:44 +0900

    create,confirm,editの定義づけ

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,37 @@
 class PostsController < ApplicationController
+
+  def index 
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.save
+      redirect_to new_post_path, notice:'つぶやきを作成しました！'
+    else
+      redirect_to new_post_path, notice:'つぶやきに失敗しました。1~140字以内で入力してください。'
+    end
+  end
+  
+  def confirm
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
 end
